67d514a86f4932066153026306e9474baa58d7d7
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-26 12:22:19 -0500

    added mutations to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,6 +3,86 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
 
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('thoughts')
+                    .populate('friends');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+
+
+        },
+
+        Mutation: {
+            addUser: async (parent, args) => {
+                const user = await User.create(args);
+                const token = signToken(user);
+
+                return { token, user };
+            },
+            login: async (parent, { email, password }) => {
+                const user = await User.findOne({ email });
+
+                if (!user) {
+                    throw new AuthenticationError('Incorrect credentials');
+                }
+
+                const correctPw = await user.isCorrectPassword(password);
+
+                if (!correctPw) {
+                    throw new AuthenticationError('Incorrect credentials');
+                }
+
+                const token = signToken(user);
+                return { token, user };
+            },
+            removeCookie: async (parent, arg, context) => {
+                if (context.user) {
+                    const updatedUser = await User.findOneAndUpdate(
+                        { _id: context.user._id },
+                        { $pull: { savedCookies: { cookieid: args.cookieId } } },
+                        // { $pull: { savedCookies: { bookid: args.bookId } } },
+                        { new: true }
+
+                    );
+                    return updatedUser;
+                }
+            },
+
+            addCookie: async (parent, args, context) => {
+                if (context.user) {
+                    const updatedUser = await User.findOneAndUpdate(
+                        { _id: context.user._id },
+                        { $push: { savedCookies: args } },
+                        { new: true }
+                    );
+                    return updatedUser;
+                }
+            },
+
+
+        }
+    }
 };
 
 module.exports = resolvers;
