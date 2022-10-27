e5932f94b742a570eea89969d41404d82ea9cb07
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-10-26 23:43:04 -0500

    added some cookies to cookie createData. mapped for cookie buttons. need to map to pull data for each individual cookie

--- client/src/Components/Cookies/index.js
+++ client/src/Components/Cookies/index.js
@@ -1,4 +1,3 @@
-import { Typography } from '@mui/material';
 import React from 'react';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
@@ -10,6 +9,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import chocolateChip from '../../assets/chocolateChip.jpg';
+import { Typography } from '@mui/material';
+// import { flexbox } from '@mui/system';
 
 const style = {
     position: 'absolute',
@@ -29,9 +30,10 @@ function createData(
     calories: number,
     fat: number,
     carbs: number,
-    protein: number
+    protein: number,
+    cost: number
 ) {
-    return { name, describe, calories, fat, carbs, protein };
+    return { name, describe, calories, fat, carbs, protein, cost };
 }
 
 // function description(
@@ -41,7 +43,9 @@ function createData(
 // }
 
 const cookies = [
-    createData('Chocolate Chip', 'Melt in your mouth delicious.', 198, 9, 28, 3),
+    createData('Chocolate Chip', 'Melt in your mouth delicious.', 198, 9, 28, 3, 26),
+    createData('Oatmeal Raisin', 'description here', 218, 9, 32, 3, 30),
+    createData('Gingersnaps', 'gingersnap description', 106, 3, 19, 1, 29)
 ];
 
 function ProjectCookies() {
@@ -53,46 +57,49 @@ function ProjectCookies() {
     return (
 
         <div className="modal-title">
-            <Button onClick={handleOpen}>{cookies[0].name}</Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
+            {cookies.map((row) => (
+                <Button onClick={handleOpen}>{row.name}</Button>
+            ))}
+                <Modal
+                    open={open}
+                    onClose={handleClose}
             >
-                <Box sx={style}>
-                    <img src={chocolateChip} width="200px" height="200px"></img>
-                    <h2>{cookies[0].describe}</h2>
-                    <TableContainer>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Cookies</TableCell>
-                                    <TableCell align="right">Calories</TableCell>
-                                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <Box sx={style}>
+                <img src={chocolateChip} width="200px" height="200px" alt=""></img>
+
+                <TableContainer>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Cookie</TableCell>
+                                <TableCell align="right">Calories</TableCell>
+                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {cookies.map((row) => (
+                                <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last=child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.carbs}</TableCell>
+                                    <TableCell align="right">{row.protein}</TableCell>
                                 </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {cookies.map((row) => (
-                                    <TableRow
-                                        key={row.name}
-                                        sx={{ '&:last=child td, &:last-child th': { border: 0 } }}
-                                    >
-                                        <TableCell component="th" scope="row">
-                                            {row.name}
-                                        </TableCell>
-                                        <TableCell align="right">{row.calories}</TableCell>
-                                        <TableCell align="right">{row.fat}</TableCell>
-                                        <TableCell align="right">{row.carbs}</TableCell>
-                                        <TableCell align="right">{row.protein}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Box>
-            </Modal>
-        </div>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <h3>Price: ${cookies[0].cost} / dozen</h3>
+            </Box>
+        </Modal>
+        </div >
     )
 }
 
