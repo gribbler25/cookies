65c4bc2cf46cc3d8b5335d9be565def75d797eaa
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-27 14:36:00 -0500

    merging

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type User {
     _id: ID
-    userName: String!
+    username: String!
     email: String!
     password: String!
     reviews: [Review]
