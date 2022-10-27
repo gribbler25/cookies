0ea782fa7a1115ec9ee2dfc1ad29e63ec7163bce
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-26 19:52:00 -0500

    merge

--- server/Models/User.js
+++ server/Models/User.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
 
+
 const userSchema = new Schema(
   {
     //don't know if I need to state this _id explicitly as ObjectId that others will reference
