9339c405ba501c975e9ceebd995614b02176890a
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-10-31 02:01:28 -0500

    added npm-react-hook-form to client. couple css changes. add signup / login forms using react-hook-form.

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -7,11 +7,11 @@ function About () {
         <div className="about">
             <div>
             <Typography>
-                <p>Quick Cookies started out as a simple coding project but has rapidly grown into a multi-million dollar online business.</p>
-                <p>With award winning, fresh cookies delivered to your door, there's no reason not to join the club!</p>
-                <p>No more wondering what to bring to the company holiday party, the neighborhood block party or your next birthday celebration!</p>
-                <p>As a member, you can sign up for regularly scheduled cookie deliveries. Select your cookies, quantity, delivery window and Quick Cookies will do the rest!</p>
-                <p>When there's nothing left but crumbs, don't forget to leave a review about your recent cookie box!</p>
+                Quick Cookies started out as a simple coding project but has rapidly grown into a multi-million dollar online business.<br></br><br></br>
+                With award winning, fresh cookies delivered to your door, there's no reason not to join the club!<br></br><br></br>
+                No more wondering what to bring to the company holiday party, the neighborhood block party or your next birthday celebration!<br></br><br></br>
+                As a member, you can sign up for regularly scheduled cookie deliveries. Select your cookies, quantity, delivery window and Quick Cookies will do the rest!<br></br><br></br>
+                When there's nothing left but crumbs, don't forget to leave a review about your recent cookie box!<br></br><br></br>
             </Typography>
             </div>
         
