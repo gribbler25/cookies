38b1ae8fd6263ed0c4c89769fd0273c2c0b47426
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-11-01 12:25:35 -0500

    merge

--- client/src/Components/SignupLogin/index.js
+++ client/src/Components/SignupLogin/index.js
@@ -1,133 +1,133 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER, ADD_USER } from '../../utils/mutations';
-import Auth from '../../utils/auth';
+// import React, { useState } from 'react';
+// import { useMutation } from '@apollo/client';
+// import { LOGIN_USER, ADD_USER } from '../../utils/mutations';
+// import Auth from '../../utils/auth';
 
-const Signup = () => {
-    const [signupFormState, setSignupFormState] = useState({ username:'', email:'', password:''});
-    const [addUser, {error}] = useMutation(ADD_USER);
+// const Signup = () => {
+//     const [signupFormState, setSignupFormState] = useState({ username:'', email:'', password:''});
+//     const [addUser, {error}] = useMutation(ADD_USER);
 
-    const handleChange =(event) =>{
-        const{ name, value} = event.target;
+//     const handleChange =(event) =>{
+//         const{ name, value} = event.target;
 
-        setSignupFormState({
-            ...signupFormState,
-            [name]:value,
-        });
-    };
+//         setSignupFormState({
+//             ...signupFormState,
+//             [name]:value,
+//         });
+//     };
 
-    //submit form
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
+//     //submit form
+//     const handleFormSubmit = async (event) => {
+//         event.preventDefault();
 
-        try{
-            const {data} = await addUser({
-                variables: {...signupFormState}
-            });
-            console.log(data)
-            Auth.login(data.addUser.token);
-        }catch(e){
-            console.error(e)
-        }
-    };
+//         try{
+//             const {data} = await addUser({
+//                 variables: {...signupFormState}
+//             });
+//             console.log(data)
+//             Auth.login(data.addUser.token);
+//         }catch(e){
+//             console.error(e)
+//         }
+//     };
 
-    return(
+//     return(
         
-        <main>
-        <h3>Sign Up!</h3>
-        <div>
-            <form onSubmit={handleFormSubmit}>
-                <input 
-                placeholder="Username"
-                name="username"
-                type="username"
-                id="username"
-                value={signupFormState.username}
-                onChange={handleChange}
-                />
-                <input
-                placeholder="Email"
-                name='email'
-                type='email'
-                id='email'
-                value={signupFormState.email}
-                onChange={handleChange}
-                />
-                <input
-                placeholder='Password'
-                name='password'
-                type='password'
-                id='password'
-                value={signupFormState.password}
-                onChange={handleChange}
-                />
-                <button type='submit'>
-                    Submit
-                </button>
-            </form>
-            {error && <div>Sign up failed</div>}
-        </div>
-        </main>
+//         <main>
+//         <h3>Sign Up!</h3>
+//         <div>
+//             <form onSubmit={handleFormSubmit}>
+//                 <input 
+//                 placeholder="Username"
+//                 name="username"
+//                 type="username"
+//                 id="username"
+//                 value={signupFormState.username}
+//                 onChange={handleChange}
+//                 />
+//                 <input
+//                 placeholder="Email"
+//                 name='email'
+//                 type='email'
+//                 id='email'
+//                 value={signupFormState.email}
+//                 onChange={handleChange}
+//                 />
+//                 <input
+//                 placeholder='Password'
+//                 name='password'
+//                 type='password'
+//                 id='password'
+//                 value={signupFormState.password}
+//                 onChange={handleChange}
+//                 />
+//                 <button type='submit'>
+//                     Submit
+//                 </button>
+//             </form>
+//             {error && <div>Sign up failed</div>}
+//         </div>
+//         </main>
        
-    );
-}
+//     );
+// }
 
-const Login = ()=>{
+// const Login = ()=>{
 
-const [loginFormState, setLoginFormState] = useState({email:'', password:''});
-const [loginUser, {error}] = useMutation(LOGIN_USER);
+// const [loginFormState, setLoginFormState] = useState({email:'', password:''});
+// const [loginUser, {error}] = useMutation(LOGIN_USER);
 
-const handleChange = (event) => {
-    const { name, value } = event.target;
+// const handleChange = (event) => {
+//     const { name, value } = event.target;
 
-    setLoginFormState({
-        ...loginFormState,
-        [name]: value,
-    });
-};
+//     setLoginFormState({
+//         ...loginFormState,
+//         [name]: value,
+//     });
+// };
 
-const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try{
-        const { data } = await loginUser({
-            variables: {...loginFormState}
-        });
-        Auth.login(data.loginUser.token);
-    }catch(e){
-        console.error(e);
-    }
-}
+// const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+//     try{
+//         const { data } = await loginUser({
+//             variables: {...loginFormState}
+//         });
+//         Auth.login(data.loginUser.token);
+//     }catch(e){
+//         console.error(e);
+//     }
+// }
 
-return(
-    <main>
-        <div>
-        <h3>Login!</h3>
-        <form onSubmit={handleFormSubmit}>
-            <input
-            placeholder="Email"
-            name='email'
-            type='email'
-            id='email'
-            value={loginFormState.email}
-            onChange={handleChange}
-            />
-            <input
-            placeholder='password'
-            name='password'
-            type='password'
-            id='password'
-            value={loginFormState.password}
-            onChange={handleChange}
-            />
-            <button type='submit'>
-                Submit
-            </button>
-        </form>
-        {error && <div>Login failed</div>}
-        </div>
-    </main>
-)
-};
+// return(
+//     <main>
+//         <div>
+//         <h3>Login!</h3>
+//         <form onSubmit={handleFormSubmit}>
+//             <input
+//             placeholder="Email"
+//             name='email'
+//             type='email'
+//             id='email'
+//             value={loginFormState.email}
+//             onChange={handleChange}
+//             />
+//             <input
+//             placeholder='password'
+//             name='password'
+//             type='password'
+//             id='password'
+//             value={loginFormState.password}
+//             onChange={handleChange}
+//             />
+//             <button type='submit'>
+//                 Submit
+//             </button>
+//         </form>
+//         {error && <div>Login failed</div>}
+//         </div>
+//     </main>
+// )
+// };
 
-export default { Signup, Login} ;
+// export default { Signup, Login} ;
 
