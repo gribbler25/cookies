faf7ec2a9f51bb9e17cf756c80499164ffa0c672
Cy Howard <cyhoward47@gmail.com>
2022-11-01 21:00:04 -0500

    redid sign up page and styled

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -1,50 +1,51 @@
 import React, { useState } from 'react';
-import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+import { Grid, Paper, Avatar, TextField, Button } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
 
-const Login = (props)=>{
+const Login = (props) => {
+    const [loginFormState, setLoginFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN_USER);
 
-    const [loginFormState, setLoginFormState] = useState({email:'', password:''});
-    const [login, {error}] = useMutation(LOGIN_USER);
-    
     const handleChange = (event) => {
         const { name, value } = event.target;
-    
+
         setLoginFormState({
             ...loginFormState,
             [name]: value,
         });
     };
-    
+
     const handleFormSubmit = async (event) => {
         event.preventDefault();
-        try{
+        
+        try {
             const { data } = await login({
-                variables: {...loginFormState}
+                variables: { ...loginFormState }
             });
             Auth.login(data.login.token);
-        }catch(e){
+        } catch (e) {
             console.error(e);
         }
     }
 
-    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#1bbd7e'}
-    const btnstyle={margin:'8px 0'}
-    return(
+    const paperStyle = { padding: 20, height: '70vh', width: 280, margin: "20px auto" }
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const btnstyle = { margin: '8px 0' }
+
+    return (
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
-                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                    <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
                     <h2>Login</h2>
                 </Grid>
                 <form onSubmit={handleFormSubmit}>
-                    <TextField label='Email' placeholder='Enter email' name='email' type='email' id='email' value={loginFormState.email} onChange={handleChange} fullWidth required/>
-                    <TextField label='Password' placeholder='Enter password' name='password' type='password' id='password' value={loginFormState.password} onChange={handleChange} fullWidth required/>
+                    <TextField label='Email' placeholder='Enter email' name='email' type='email' id='email' value={loginFormState.email} onChange={handleChange} fullWidth required />
+                    <TextField label='Password' placeholder='Enter password' name='password' type='password' id='password' value={loginFormState.password} onChange={handleChange} fullWidth required />
                     <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Login</Button>
                 </form>
                 {error ? <div>Login failed</div> : null}
@@ -53,5 +54,4 @@ const Login = (props)=>{
     )
 };
 
-    export default Login;
-    
+export default Login;
--- client/src/Components/Signup/index.js
+++ client/src/Components/Signup/index.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
+import { Grid, Paper, Avatar, TextField, Button } from '@material-ui/core'
+import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
 
 const Signup = () => {
-    const [signupFormState, setSignupFormState] = useState({ username:'', email:'', password:''});
-    const [addUser, {error}] = useMutation(ADD_USER);
+    const [signupFormState, setSignupFormState] = useState({ username: '', email: '', password: '' });
+    const [addUser, { error }] = useMutation(ADD_USER);
 
-    const handleChange =(event) =>{
-        const{ name, value} = event.target;
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
         setSignupFormState({
             ...signupFormState,
-            [name]:value,
+            [name]: value,
         });
     };
 
@@ -21,55 +23,37 @@ const Signup = () => {
     const handleFormSubmit = async (event) => {
         event.preventDefault();
 
-        try{
-            const {data} = await addUser({
-                variables: {...signupFormState}
+        try {
+            const { data } = await addUser({
+                variables: { ...signupFormState }
             });
             console.log(data)
             Auth.login(data.addUser.token);
-        }catch(e){
+        } catch (e) {
             console.error(e)
         }
     };
 
-    return(
-        
-        <main>
-        <h3 id="signup-title">Sign Up!</h3>
-        <div>
-            <form onSubmit={handleFormSubmit}>
-                <input 
-                placeholder="Username"
-                name="username"
-                type="username"
-                id="username"
-                value={signupFormState.username}
-                onChange={handleChange}
-                />
-                <input
-                placeholder="Email"
-                name='email'
-                type='email'
-                id='email'
-                value={signupFormState.email}
-                onChange={handleChange}
-                />
-                <input
-                placeholder='Password'
-                name='password'
-                type='password'
-                id='password'
-                value={signupFormState.password}
-                onChange={handleChange}
-                />
-                <button type='submit'>
-                    Submit
-                </button>
-            </form>
-            {error ? <div>Sign up failed</div> : null}
-        </div>
-        </main>
-       
+    const paperStyle = { padding: 20, height: '70vh', width: 280, margin: "20px auto" }
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const btnstyle = { margin: '8px 0' }
+
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LockOpenOutlinedIcon /></Avatar>
+                    <h2>Sign Up</h2>
+                </Grid>
+                <form onSubmit={handleFormSubmit}>
+                    <TextField label='Username' name='username' type='username' id='username' value={signupFormState.username} onChange={handleChange} fullWidth required />
+                    <TextField label='Email' name='email' type='email' id='email' value={signupFormState.email} onChange={handleChange} fullWidth required />
+                    <TextField label='Password' name='password' type='password' id='password' value={signupFormState.password} onChange={handleChange} fullWidth required />
+                    <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign Up</Button>
+                </form>
+                {error ? <div>Sign up failed</div> : null}
+            </Paper>
+        </Grid>
     );
 };
 
